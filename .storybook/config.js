710adddcbc8d771da51281ce2992874165ac5300
Chris Chou <chhschou@gmail.com>
2019-01-06 23:03:12 +1300

    fix lit-html render in storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,25 @@
-import { configure } from '@storybook/polymer';
+import { configure, addDecorator } from '@storybook/polymer';
+import { document } from 'global';
+import { render } from 'lit-html';
+
+const litDecorator = (story, context, param) => {
+  const component = story();
+  if (!(component instanceof HTMLElement)) {
+    const el = document.createElement('section'); // this element probably can br reused.
+    render(component, el);
+    return el;
+  }
+
+  return story();
+};
+addDecorator(litDecorator);
 
 const req = require.context('../components', true, /\.stories.js$/);
 
 function loadStories() {
-  req.keys().forEach(filename => req(filename));
+  const contexts = req.keys();
+  console.log('context', contexts);
+  contexts.forEach((filename) => req(filename));
 }
 
 configure(loadStories, module);
